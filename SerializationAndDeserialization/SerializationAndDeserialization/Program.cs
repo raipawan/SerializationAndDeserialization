ee2dbaf77d022cdb4a6dcc4265fa607cc4daeba1
pavan kumar <raipawan337@gmail.com>
2021-12-09 12:35:17 +0530

    [Pavan]Added: Binaryserialization
    exit

---
+++ SerializationAndDeserialization/SerializationAndDeserialization/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SerializationAndDeserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BinarySerialization binarySerialization = new BinarySerialization();
+            binarySerialization.Serialization();
+        }
+    }
+}
